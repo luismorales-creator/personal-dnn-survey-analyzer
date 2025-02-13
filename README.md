b2b226a7912e31c479d89c487f52e2324a9e7bf6
luismorales-creator <lema1202@gmail.com>
2025-02-13 12:01:56 -0500

    New code for printing combined labels and values

---
+++ README.md
@@ -0,0 +1,21 @@
+# Personal DNN Surveys Analyzer
+
+## Requirements
+- Python (version 3.9 or later)
+- Pandas 
+
+## Subheading
+
+This is **bold**
+
+This is *italics*
+
+- This is an unordered list
+- Blah blah
+
+1. Numbered list
+2. Again!
+
+## TODOS
+
+- Add pyenv 
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import json
+from tabulate import tabulate
+pd.set_option('display.max_columns', None)  # Show all columns
+pd.set_option('display.width', None)        # Don't wrap wide displays
+pd.set_option('display.max_rows', None)     # Show all rows
 
 print("starting the dnn file transformer")
 
@@ -10,9 +15,24 @@ session_2_labels = pd.read_csv('data/session_2/labels.csv', skiprows = [1,2]) # 
 session_2_values = pd.read_csv('data/session_2/values.csv', skiprows = [1,2]) # <--- this is called a dataframe
 
 # print a preview of the dataframe
-print(session_1_labels)
-print(session_1_values)
+# print(session_1_labels.head(n=5))
+# print("#######")
+# print(session_1_values.head(n=5))
 
-# might need?
-# if __name__ == "__main__":
-#     print("hi Luis")
+combined_df = pd.merge(session_1_labels, session_1_values, on="ResponseId", how="inner")
+# filtered_columns = ["StartDate_x", "IPAddress_x", "ResponseId"]
+# print("all the columns: ", combined_df.columns)
+# print(combined_df[filtered_columns])
+original_columns = combined_df.columns
+combined_with_null_columns_removed_df = combined_df.dropna(axis=1, how="all")
+cleaned_columns = combined_with_null_columns_removed_df.columns
+
+# print("Columns with only nulls", set(original_columns) - set(cleaned_columns)) # <----this prints the null columns
+output_filename = "combined_with_null_columns_removed_df.csv"
+combined_with_null_columns_removed_df.to_csv(output_filename, index=False)
+
+# print(tabulate(combined_df))
+# combined_df.to_csv("combined.csv", index=False)
+
+
+
